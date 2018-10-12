bcd4ed003df8faf32f5015b06943ce0e90da17eb
Mark Heijnekamp <26150@ma-web.nl>
2018-10-12 18:08:03 +0200

    intger

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+        Integer hElP = new Integer (33);
+
+        System.out.println(hElP);
+
+        Integer heLlo;
+
+        heLlo = (16093440);
+
+        System.out.println(heLlo);
+
+        int bYe = Integer.parseInt("123");
+
+        System.out.println(bYe);
+
+
+    }
+
+}
